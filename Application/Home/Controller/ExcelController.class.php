469ba811680853fdd4f73f61a9b3136997fc793b
noFloat <304546210@qq.com>
2016-02-13 13:19:05 +0800

    update

--- Application/Home/Controller/ExcelController.class.php
+++ Application/Home/Controller/ExcelController.class.php
@@ -1,16 +1,7 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
-/**
-* @name Excel
-* @access 教务处、root
-* @author 丛广林
-* @const 指明常量
-* @module Home
-* @throws linux 未设置public文件夹中Excel和Csv文件夹权限会报错
-* @version 1.0
-*/
-class ExcelController extends BaseController {
+class ExcelController extends Controller {
 
 
 
@@ -18,18 +9,6 @@ class ExcelController extends BaseController {
     	$this->display('');
     }
 
-    /**
-    * @name showExcel
-    * @access 教务处、root
-    * @const 指明常量
-    * @module Home
-    * @param stunum传入学号为,,分割的字符串
-    * @return 返回csv文件
-    * @throws linux 未设置Public文件夹中Excel和Csv文件夹权限会报错
-    * @todo 前端未作ajaxform，用户体验想对一般
-    * @var 定义说明变量/属性。
-    * @version 1.0
-    */
     public function showExcel(){
     	$student = M('student');
     	$goal_id = explode(',',I('post.stunum'));
@@ -65,18 +44,7 @@ class ExcelController extends BaseController {
     public function export(){
     	$this->display('');
     }
-    /**
-    * @name import
-    * @access 教务处、root
-    * @const 指明常量
-    * @module Home
-    * @param 传入xlsx或xls文件
-    * @return 
-    * @throws linux 未设置Public文件夹中Excel和Csv文件夹权限会报错
-    * @todo 前端未作ajaxform，用户体验想对一般；文件大小未设置限制，需上线后考虑
-    * @var phpExcel实例化phpexcel插件
-    * @version 1.0
-    */
+
     public function import(){  
     	$upload = new \Think\Upload();
 		$upload->maxSize = 0;
@@ -99,7 +67,7 @@ class ExcelController extends BaseController {
             $PHPReader=new \PHPExcel_Reader_Excel5();
         }else{
             import("Org.Util.PHPExcel.Reader.Excel2007");
-        $PHPReader=new \PHPExcel_Reader_Excel2007();
+            $PHPReader=new \PHPExcel_Reader_Excel2007();
         }
 		$PHPExcel=$PHPReader->load($filename);
 		//获取表中的第一个工作表，如果要获取第二个，把0改为1，依次类推
@@ -108,16 +76,17 @@ class ExcelController extends BaseController {
 		$allColumn=$currentSheet->getHighestColumn();
 		//获取总行数
 		$allRow=$currentSheet->getHighestRow();
+        if($allColumn!='D'){
+            $this->error("导入文件格式有误");
+        }
 		//循环获取表中的数据，$currentRow表示当前行，从哪行开始读取数据，索引值从0开始
 		$sql_standard = array(
-				A => "stu_name",
-				B => "stu_id",
-				C => "sex",
-				D => "class_id",
-				E => "idcard",
-				F => "province",
+				A => "bookname",
+				B => "num",
+				C => "pubhouse",
+				D => "auth",
 			);
-		for($currentRow=1;$currentRow<=$allRow;$currentRow++){
+		for($currentRow=0;$currentRow<=$allRow;$currentRow++){
 			//从哪列开始，A表示第一列
 			for($currentColumn='A';$currentColumn<=$allColumn;$currentColumn++){
 				//数据坐标
@@ -128,18 +97,18 @@ class ExcelController extends BaseController {
 		
 		}
 		array_shift($arr);
-		$excel =  M('student');
+		$excel =  M('book');
 		$num = count($arr);
+        $restnum = 0;
         foreach($arr as $key => $value){
         	$condition = array(
-        			"stu_id" => $arr[$key]['stu_id'],
+        			"bookname" => $arr[$key]['stu_id'],
         		);
-            $salt = mt_rand(10,200000);
-            $value['salt'] = $salt;
-            $value['password'] = md5(hash('sha256', ($salt % 3))).sha1(substr($value['idcard'],-6));
+            $value['purchingtime'] = date("Y-m-d", time());
         	$output = $excel->where($condition)->find();
         	//var_dump($output);
         	if($output){
+                $resttime += 1;
             	$goal = $excel->where($condition)->data($value)->lock(true)->save();//悲观锁
         	}else{
         		$goal = $excel->add($value);
@@ -150,51 +119,41 @@ class ExcelController extends BaseController {
         		"info"  => "success",
                 "state" => 200,
         	);
-            $all_student = $excel->select();
-            foreach ($all_student as $key => $student_value) {
-                S($student_value['stu_id'],$student_value);
-            }
-            S('stu',1);
+            // $all_student = $excel->select();
+            // foreach ($all_student as $key => $student_value) {
+            //     S($student_value['stu_id'],$student_value);
+            // }
+            // S('stu',1);
             $log = D('Log');
             $log->addLog('导入学生');
-            $this->success("添加成功");
+            $this->success("添加成功,重复数目为".$restnum);
         }else{
         	$info = array(
                     "info"  => "failed",
                     "state" => 404,
             );
-            $this->error("添加失败");
+            $this->error("添加失败或数据库无更新");
         }
     }
-
-    /**
-    * @name searchStudent
-    * @access 教务处、root
-    * @const 指明常量
-    * @module Home
-    * @param stunum=>学号,class_id=>班级号,province=>省份
-    * @return $info[ "info"  => "xxxx",
-                    "state" => x0x,
-                    "data"  => $goal_student,
-                    ]
-    * @throws 
-    * @todo 
-    * @var 
-    * @version 1.0
-    */
-    public function searchStudent(){
-        $student = M('student');
-        if(S('stu') == null){
-            $all_student = $student->select();
-            foreach ($all_student as $key => $value) {
-                S($value['stu_id'],$value);
-            }
-            S('stu',1);
-        }
-    	if(I('post.stunum')!=null){
-    		$goal_student = S(I('post.stunum'));
-            $goal_studentnow[0] = $goal_student;
-            if(!$goal_student){
+    public function searchBook(){
+        $book = M('book');
+        // if(S('stu') == null){
+        //     $all_student = $book->select();
+        //     foreach ($all_student as $key => $value) {
+        //         S($value['stu_id'],$value);
+        //     }
+        //     S('stu',1);
+        // }
+        $postbook = I('post.bookname');
+    	if($postbook!=null){
+    		// $goal_student = S(I('post.stunum'));
+            $condition = array(
+                    "bookname" => array('like',"%$postbook%"),
+                    "num" => array('gt',"0"),
+                );
+            $goal_book = $book->where($condition)->field('id,bookname,purchingtime,num,pubhouse,auth')->select();
+            $goal_studentnow = $goal_book;
+            if(!$goal_book){
                 $info = array(
                         "info"  => "failed",
                         "state" => 404,
@@ -209,14 +168,17 @@ class ExcelController extends BaseController {
                 echo json_encode($info);exit;
             }
     	}
-    	if(I('post.classid')!=null){
-    		$condition['class_id'] = I('post.classid');
-    	}
-    	if(I('post.province')!=null){
-    		$condition['province'] = I('post.province');
-    	}
-    	$goal_student = $student->where($condition)->select();
-    	if(!$goal_student){
+    	if(I('post.id')!=null){
+    		$condition['id'] = I('post.id');
+    	}else{
+            $info = array(
+                    "info"  => "请输入书名",
+                    "state" => 404,
+                );
+            echo json_encode($info);exit;
+        }
+    	$goal_book = $book->where($condition)->find();
+    	if(!$goal_book){
     		$info = array(
                     "info"  => "failed",
                     "state" => 404,
@@ -226,55 +188,40 @@ class ExcelController extends BaseController {
     		$info = array(
                     "info"  => "success",
                     "state" => 200,
-                    "data"  => $goal_student,
+                    "data"  => $goal_book,
                 );
             echo json_encode($info);
     	}
     }
 
-    /**
-    * @name update
-    * @access 教务处、root
-    * @const 指明常量
-    * @module Home
-    * @param stunum=>学号,sex=>性别,status=>状态,class_id=>班级号,province=>省份
-    * @return 
-    * @throws 
-    * @todo 换成ajaxform,更新判断多样化
-    * @var 
-    * @version 1.0
-    */
+
     public function update(){
-        $student = M('student');
+        $book = M('book');
         $content = array(
-                "stu_name" => I('post.stu_name'),
-                "sex"  => I('post.stu_sex'),
-                "class_id" => I('post.stu_class'),
-                "province" => I('post.stu_province'),
-                "status"   => I('post.status'),
+                "bookname" => I('post.bookname'),
+                "num"      => I('post.num'),
+                "pubhouse" => I('post.pubhouse'),
+                "auth"     => I('post.auth'),
             );
         $condition = array(
-                "stu_id" => I('post.stu_id')
+                "id" => I('post.id')
             );
-        $goal = $student->where($condition)->data($content)->save();
-        if($goal){
-            $this->success('修改成功');
+        $goal = $book->where($condition)->data($content)->save();
+        if(!$goal){
+            $info = array(
+                    "info"  => "failed",
+                    "state" => 404,
+                );  
+            echo json_encode($info);
         }else{
-            $this->error("修改失败");
+            $info = array(
+                    "info"  => "success",
+                    "state" => 200,
+                );
+            echo json_encode($info);
         }
     }
-    /**
-    * @name addAnnex
-    * @access 教务处、root
-    * @const 指明常量
-    * @module Home
-    * @param 传入doc、docx、zip
-    * @return 
-    * @throws linux 未设置Public文件夹中EAnnex文件夹权限会报错
-    * @todo 赞暂不支持多文件上传;前端未作ajaxform，用户体验想对一般；文件大小未设置限制，需上线后考虑
-    * @var $log实例化Model添加日志
-    * @version 1.0
-    */
+
     public function addAnnex(){
         $upload = new \Think\Upload();
         $upload->maxSize = 0;
@@ -305,55 +252,45 @@ class ExcelController extends BaseController {
             $this->error("添加失败");
         }
     }
-    /**
-    * @name searchStudent
-    * @access 教务处、root
-    * @const 指明常量
-    * @module Home
-    * @param stunum=>学号,class_id=>班级号,province=>省份，idcard=>身份证,salt=>加密盐
-    * @return $info[ "info"  => "xxxx",
-                    "state" => x0x,
-                    ]
-    * @throws 
-    * @todo 判断多样化
-    * @var 
-    * @version 1.0
-    */
+
     public function addOne(){
         $salt = mt_rand(10,200000);
         $content = array(
-            'stu_name' => I('post.addone_name'),
-            'sex'      => I('post.addone_sex'),
-            'stu_id'    => I('post.addone_stuid'),
-            'province' => I('post.addone_province'),
-            'class_id' => I('post.addone_classid'),
-            'idcard'   => I('post.addone_idcard'),
-            'password' => md5(hash('sha256', ($salt % 3))).sha1(substr(I('post.addone_idcard'),-6)),
-            'salt'     => $salt,
-
+            'bookname'     => I('post.addone_bookname'),
+            'num'          => I('post.addone_num'),
+            'pubhouse'     => I('post.addone_pubhouse'),
+            'auth'         => I('post.addone_auth'),
+            'purchingtime' => date("Y-m-d H:i:s", time()),
         );
-        foreach($content as $key => $value){
-            if($value = null){
-                $info = array(
+        if(in_array("",$content) != 0){
+            $info = array(
                     "info"  => "请保证数据无空值",
                     "state" => 404,
                 );
-                echo json_encode($info);
-            }
+            echo json_encode($info);exit;
         }
-        $student = M('student');
+        // foreach($content as $key => $value){
+        //     if($value = null){
+        //         $info = array(
+        //             "info"  => "请保证数据无空值",
+        //             "state" => 404,
+        //         );
+        //         echo json_encode($info);
+        //     }
+        // }
+        $book = M('book');
         $condition = array(
-                "stu_id" => I('post.addone_stuid')
+                "bookname" => I('post.addone_bookname')
             );
-        $goal_student = $student->where($condition)->find();
-        if($goal_student){
+        $goal_book = $book->where($condition)->find();
+        if($goal_book){
             $info = array(
-                    "info"  => "该学生已存在，添加失败",
+                    "info"  => "该书已存在，添加失败",
                     "state" => 404,
                 );
             echo json_encode($info);
         }else{
-            $student->add($content);
+            $book->add($content);
             $info = array(
                 "info"  => "添加成功",
                 "state" => 200,
@@ -361,6 +298,8 @@ class ExcelController extends BaseController {
             echo json_encode($info);
         }
     }
+
+
 
     public function _empty() {
         $this->display('404/index');
